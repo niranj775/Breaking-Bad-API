06b958bee019fda56ca3a656f3cad3307ff267f8
niranj775 <niranj775@gmail.com>
2021-08-04 19:42:54 +0530

    04-08-2021

--- script.js
+++ script.js
@@ -1,53 +1,73 @@
-const url = "https://www.breakingbadapi.com/api/";
-
-async function getApiData(url) {
+async function getChar() {
   try {
-    let response = await fetch(url);
+    let response = await fetch("https://www.breakingbadapi.com/api/characters");
     let data = await response.json();
-    for (let e in data) {
-      let tag = document.createElement("span");
-      tag.setAttribute("class", "navbar-brand mb-0 h1");
-      tag.setAttribute("onclick", e + "()");
-      tag.innerHTML = e.charAt(0).toUpperCase() + e.slice(1);
 
-      let nav = document.querySelector("nav");
-      nav.append(tag);
-    }
+    let eleFound = false;
+    let name = document.querySelector("input").value;
+
+    data.forEach((ele) => {
+      if (ele.name.toLowerCase() == name.toLowerCase()) {
+        eleFound = true;
+        let container = document.getElementById("container");
+        container.innerHTML = "";
+
+        let row = createEle("div", "row");
+
+        let col = createEle("div", "col");
+        col.setAttribute("class", "mr-auto ml-auto");
+
+        let card = createEle("div", "card border border-info");
+        card.setAttribute("style", "width: 18rem;");
+
+        let img = createEle("img", "card-img-top h-25");
+        img.setAttribute("alt", "Character Image");
+        img.setAttribute("src", ele.img);
+
+        let cardBody = createEle("div", "card-body");
+
+        let cardTitle = createEle(
+          "h5",
+          "card-title bg-info text-white d-flex justify-content-center"
+        );
+        cardTitle.innerHTML = ele.name;
+
+        let cardText1 = createEle(
+          "h6",
+          "card-text d-flex justify-content-center"
+        );
+        cardText1.innerHTML = `Nickname: ${ele.nickname}`;
+
+        let cardText2 = createEle(
+          "h6",
+          "card-text d-flex justify-content-center"
+        );
+        cardText2.innerHTML = `Status: ${ele.status}`;
+
+        let cardText3 = createEle(
+          "h6",
+          "card-text d-flex justify-content-center"
+        );
+        cardText3.innerHTML = `Actor: ${ele.portrayed}`;
+
+        cardBody.append(cardTitle, cardText1, cardText2, cardText3);
+        card.append(img, cardBody);
+
+        col.append(card);
+        row.append(col);
+
+        container.append(row);
+
+        function createEle(elem, value) {
+          let tag = document.createElement(elem);
+          tag.setAttribute("class", value);
+          return tag;
+        }
+      }
+    });
+
+    if (eleFound == false) alert("Character not found!");
   } catch (err) {
     console.log(err);
-  }
-}
-
-getApiData(url);
-
-async function characters() {
-  try {
-    let charResp = await fetch(`${url}characters`);
-    let charData = await charResp.json();
-    charData.forEach((element) => {
-      let card = document.createElement("div");
-      card.setAttribute("class", "card");
-      card.setAttribute("style", "card");
-
-      let img = document.createElement("img");
-      img.setAttribute("class", "card-img-top");
-      img.setAttribute("src", element.image);
-      img.setAttribute("alt", element.name);
-
-      let cardBody = document.createElement("div");
-      cardBody.setAttribute("class", "card-body");
-
-      let cardText = document.createElement("p");
-      cardText.setAttribute("class", "card-text");
-      cardText.innerHTML = element.name;
-
-      cardBody.append(cardText);
-      card.append(card, img);
-
-      let content = document.getElementById("content");
-      content.append(card);
-    });
-  } catch (error) {
-    console.log(error);
   }
 }
